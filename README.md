3cc319cf19e460a2b16abc23417c6d6d77a01623
caojs <caoo.dai@gmail.com>
2018-12-11 00:41:52 +0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# mserver
+
+## Development
+* On MacOS or Linux
+$ DEBUG=myapp:* npm start
+
+* On Windows, use this command:
+> set DEBUG=myapp:* & npm start
+
+## Production
+npm start
---
+++ public/precache-manifest.f38a093d0f0bf433ebdc4e56f1d9913e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36ba85d10e464e98e32b",
+    "url": "/static/js/main.36ba85d1.chunk.js"
+  },
+  {
+    "revision": "b44601a45312dc587516",
+    "url": "/static/js/1.b44601a4.chunk.js"
+  },
+  {
+    "revision": "36ba85d10e464e98e32b",
+    "url": "/static/css/main.1157e0a8.chunk.css"
+  },
+  {
+    "revision": "b44601a45312dc587516",
+    "url": "/static/css/1.a0f96e51.chunk.css"
+  },
+  {
+    "revision": "f8aa8f44f68d6faaf67abe3845af4f8e",
+    "url": "/index.html"
+  }
+];
